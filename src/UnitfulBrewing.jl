940bb0823126e6f16afa4d53dacad0ba992c3a7e
Ricardo Rosa <rmsrosa@gmail.com>
2021-01-25 13:16:31 -0300

    fix lack of promotion of new dimensions

--- src/UnitfulBrewing.jl
+++ src/UnitfulBrewing.jl
@@ -251,6 +251,11 @@ function show_quantity_info(x::Quantity{T,D,U}) where {T,D,U}
 end
 
 # Register the above units and dimensions in Unitful
-__init__() = Unitful.register(UnitfulBrewing)
+const localpromotion = Unitful.promotion # only needed with new dimensions
+function __init__()
+    Unitful.register(UnitfulBrewing) # needed for new Units
+    merge!(Unitful.promotion, localpromotion) # only needed with new dimensions
+end
+
 
 end # module
